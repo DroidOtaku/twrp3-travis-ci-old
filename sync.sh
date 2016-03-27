70b635c59ce300148b6a5657a949f69f2621adee
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-03-27 20:39:23 -0300

    sync.sh: Make it script

--- sync.sh
+++ sync.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-
 loop() {
 LEND=$(date +"%s")
 LBUILDTIME=$(($LEND - $START))
 echo -ne "\r\033[K"
-echo -ne "${bldgrn}Build Time: $(($LBUILDTIME / 60)) minutes and $(($LBUILDTIME % 60)) seconds.${txtrst}"
+echo -ne "${bldgrn}Sync Time: $(($LBUILDTIME / 60)) minutes and $(($LBUILDTIME % 60)) seconds.${txtrst}"
 if ! [ -f arch/$ARCH/boot/zImage ]; then
 	sleep 1
 	loop
 fi
 }
+START=$(date +"%s")
+repo sync -q -j${NR_CPUS} &>/dev/null | loop
+rm -rf .repo
