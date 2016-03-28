fc13ca8d9b5569394efc100423a1dd4f8b7247c0
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-03-27 21:02:46 -0300

    sync.sh: Temp

--- sync.sh
+++ sync.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
-sudo apt-get install parallel
-loop() {
-LEND=$(date +"%s")
-LBUILDTIME=$(($LEND - $LSTART))
-echo -ne "\r\033[K"
-echo -ne "${bldgrn}Sync Time: $(($LBUILDTIME / 60)) minutes and $(($LBUILDTIME % 60)) seconds.${txtrst}"
-if ! [ "$(($LBUILDTIME / 60))" == "10"  ]; then
-	sleep 1
-	loop
-fi
-}
-LSTART=$(date +"%s")
-( echo repo sync -q -j${NR_CPUS}; echo loop) | parallel
+repo sync -q -j1
 rm -rf .repo
